2468a84130aaf3ac0f9067215f896f1fd0e4314e
风舒云卷-北斗不移 <948247738@qq.com>
2019-03-28 11:27:30 +0800

    ok
    ok

---
+++ command/start_yarn_monitor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+################### opm #######################
+#cd /home/gaojr/yarn-monitor/
+#/home/gaojr/Python-2.7.15/python YarnMonitor.py
+################################################
+
+################### opt ########################
+cd /bigdata/yarnmonitor/yarn-monitor
+python YarnMonitor.py
+################################################
